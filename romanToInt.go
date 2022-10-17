d5460d269a076e17ea1e6cb4bc6e09175166a9e7
AlexeyBorisovets <borisan4@gmail.com>
2022-10-17 22:32:39 +0300

    add: romanToIntFunction

---
+++ romanToInt.go
@@ -0,0 +1,22 @@
+package main
+
+func romanToInt(s string) int {
+	roman := map[string]int{
+		"I": 1,
+		"V": 5,
+		"X": 10,
+		"L": 50,
+		"C": 100,
+		"D": 500,
+		"M": 1000,
+	}
+	var result int
+	for i := 0; i < len(s); i++ {
+		if i+1 < len(s) && roman[string(s[i])] < roman[string(s[i+1])] {
+			result -= roman[string(s[i])]
+		} else {
+			result += roman[string(s[i])]
+		}
+	}
+	return result
+}
